caad3c117c4ede389ce492058f4b3ceb663f21e9
Jonas Bünning <buenning@mbd.rwth-aachen.de>
2026-06-26 13:55:09 +0200

    grep with relative to current file plus absolute grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -74,7 +74,7 @@ return {
       vim.keymap.set("n", "<leader>sp", builtin.git_files, { desc = "[S]earch [P]roject (Git)" })
       vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
       vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-      vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+      vim.keymap.set("n", "<leader>sG", builtin.live_grep, { desc = "[S]earch by [G]rep (current dir)" })
       vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
       vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
       vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -106,6 +106,10 @@ return {
       vim.keymap.set("n", "<leader>sf", function()
         builtin.find_files({ cwd = vim.fn.expand("%:p:h") })
       end, { desc = "[S]earch [F]iles (from curr dir)" })
+
+      vim.keymap.set("n", "<leader>sg", function()
+        builtin.live_grep({ cwd = vim.fn.expand("%:p:h") })
+      end, { desc = "[S]earch by [G]rep" })
 
       vim.keymap.set("n", "<leader>so", function()
         builtin.live_grep({ cwd = "$FOAM_SRC" })
