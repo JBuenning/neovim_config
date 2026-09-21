68802853d793f9cecabda206c99c9363f56d9e7f
Jonas Bünning <buenning@mbd.rwth-aachen.de>
2026-09-21 20:29:38 +0200

    make blink default completion

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,40 +1,22 @@
-require("lazy").setup({
-  { "NMAC427/guess-indent.nvim", opts = {} }, -- Detect tabstop and shiftwidth automatically
-  require("plugins/treesitter"),
-  require("plugins/which-key"),
-  require("plugins/telescope"),
-  require("plugins/mini"),
-  require("plugins/gitsigns"),
-  require("plugins/lspconfig"),
-  require("plugins/conform"),
-  require("plugins/cmp"),
-  require("plugins/colorthemes"),
-  require("plugins/todo-comments"),
-  require("plugins/colorizer"),
+require('lazy').setup {
+  { 'NMAC427/guess-indent.nvim', opts = {} }, -- Detect tabstop and shiftwidth automatically
+  require 'plugins/treesitter',
+  require 'plugins/which-key',
+  require 'plugins/telescope',
+  require 'plugins/mini',
+  require 'plugins/gitsigns',
+  require 'plugins/lspconfig',
+  require 'plugins/conform',
+  -- require 'plugins/cmp',
+  require 'plugins/blink-cmp',
+  require 'plugins/colorthemes',
+  require 'plugins/todo-comments',
+  require 'plugins/colorizer',
   -- require("plugins/autopairs"),
-  require("plugins/oil"),
-  require("plugins/vim-tmux-navigator"),
-  require("plugins/dap"),
+  require 'plugins/oil',
+  require 'plugins/vim-tmux-navigator',
+  require 'plugins/dap',
   -- require("plugins/quarto"),
-  require("plugins/slime"),
-}, {
-  ui = {
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = "⌘",
-      config = "🛠",
-      event = "📅",
-      ft = "📂",
-      init = "⚙",
-      keys = "🗝",
-      plugin = "🔌",
-      runtime = "💻",
-      require = "🌙",
-      source = "📄",
-      start = "🚀",
-      task = "📌",
-      lazy = "💤 ",
-    },
-  },
-})
+  require 'plugins/slime',
+}
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,74 @@
+return {
+  { -- Autocompletion
+    'saghen/blink.cmp',
+    dependencies = {
+      'saghen/blink.lib',
+      'L3MON4D3/LuaSnip',
+      -- optional: provides snippets for the snippet source
+      -- 'rafamadriz/friendly-snippets',
+    },
+    build = function()
+      -- build the fuzzy matcher, optionally add a timeout to `pwait(timeout_ms)`
+      -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
+      require('blink.cmp').build():pwait()
+    end,
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = {
+        -- 'default' (recommended) for mappings similar to built-in completions
+        --   <c-y> to accept ([y]es) the completion.
+        --    This will auto-import if your LSP supports it.
+        --    This will expand snippets if the LSP sent a snippet.
+        -- 'super-tab' for tab to accept
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- All presets have the following mappings:
+        -- <tab>/<s-tab>: move to right/left of your snippet expansion
+        -- <c-space>: Open menu or open docs if already open
+        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+        -- <c-e>: Hide menu
+        -- <c-k>: Toggle signature help
+        --
+        -- See `:help blink-cmp-config-keymap` for defining your own keymap
+        preset = 'default',
+
+        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      },
+
+      -- appearance = {
+      --   -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      --   -- Adjusts spacing to ensure icons are aligned
+      --   nerd_font_variant = 'mono',
+      -- },
+
+      completion = {
+        -- By default, you may press `<c-space>` to show the documentation.
+        -- Optionally, set `auto_show = true` to show the documentation after a delay.
+        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      },
+
+      -- sources = {
+      --   default = { 'lsp', 'path', 'snippets' },
+      -- },
+
+      snippets = { preset = 'luasnip' },
+
+      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+      -- which automatically downloads a prebuilt binary when enabled.
+      --
+      -- By default, we use the Lua implementation instead, but you may enable
+      -- the rust implementation via `'prefer_rust_with_warning'`
+      --
+      -- See `:help blink-cmp-config-fuzzy` for more information
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
+
+      -- Shows a signature help window while you type arguments for a function
+      signature = { enabled = true },
+    },
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
